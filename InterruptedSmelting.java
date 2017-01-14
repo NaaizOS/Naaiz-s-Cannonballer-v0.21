722f5c0aaba5e21a6a745f2793dccaf694f141f1
W1-BergS <stan.vandenberg2@edu-kw1c.nl>
2017-01-14 15:01:02 +0100

    Update InterruptedSmelting.java

--- InterruptedSmelting.java
+++ InterruptedSmelting.java
@@ -10,31 +10,23 @@ public class InterruptedSmelting extends Task {
 	
 	@Override
 	public boolean canProcess() {
-		return api.widgets.getWidgetContainingText("Click here to continue") != null && !api.inventory.isItemSelected() && steelBars <= 26;
+		return api.widgets.getDialogues().isPendingContinuation() && !api.inventory.isItemSelected() && steelBars <= 26;
 	}
 
 	@Override
 	public void process() {
 			api.dialogues.clickContinue();
 			api.inventory.getItem("Steel bar").interact("Use");
-			if(api.inventory.isItemSelected())
+			if(api.inventory.isItemSelected()){
 				api.getObjects().closest("Furnace").interact("Use");
+			}
 			api.mouse.moveRandomly();
-			new ConditionalSleep(400, 200) {
+			new ConditionalSleep(3000) {
 				@Override
 	            public boolean condition() throws InterruptedException {
-	                return api.getObjects().closest("Furnace").interact("Use");
+					return api.widgets.getWidgetContainingText("Steel bar") != null && api.widgets.getWidgetContainingText("Steel bar").isVisible();
 	            }
 			}.sleep();
-			if(api.inventory.isItemSelected()) {
-			
-			}
-			new ConditionalSleep(6000, 2000) {
-		            @Override
-		            public boolean condition() throws InterruptedException {
-		                return api.widgets.getWidgetContainingText("Steel bar") != null;
-		            }
-		        }.sleep();
 		        if(api.myPlayer().getPosition().getX() == 3109 && api.myPlayer().getPosition().getY() == 3499) {
 			        api.widgets.interact(309, 6, "Make All");
 			        api.mouse.moveOutsideScreen();
